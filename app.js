772b889597bc1ffbc7a13f9875e5b74baee39747
robonexx <robertwagar@gmail.com>
2022-04-05 06:35:06 +0200

    ...

---
+++ app.js
@@ -0,0 +1,20 @@
+console.log('js connected')
+
+const btnScrollToTop = document.querySelector('.btnScrollToTop')
+const btnReadMore = document.querySelector('.btnReadMore')
+
+let w = window.innerWidth;
+let h = window.innerHeight;
+document.querySelector(".showHeight").innerHTML = "Width: " + w + "<br>Height: " + h;
+
+
+btnScrollToTop.addEventListener('click', () => {
+    console.log('btn clicked')
+    window.scrollTo(0,0)
+})
+btnReadMore.addEventListener('click', () => {
+    console.log('btn clicked')
+    btnReadMore.style.transform = 'translateY(-3px)'
+    const bottomSection = h * 3
+    window.scrollBy(0,bottomSection)
+})
